d9aa66ac33f23e29a7aa0c65ce12d2c14bdfc4f5
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-10-12 11:56:35 -0300

    ajuste de banco

--- src/main/java/br/com/wellingtonmonteiro/todolist/user/UserModel.java
+++ src/main/java/br/com/wellingtonmonteiro/todolist/user/UserModel.java
@@ -1,11 +1,31 @@
 package br.com.wellingtonmonteiro.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+// import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+// O @Entity mostra que tudo dentro dessa entity será uma tabela com essas colunas
 @Data
+@Entity(name = "tb-users")
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    // Especificando qual o nome da coluna 
+    // @Column(name = "usuario")
     private String userName;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
