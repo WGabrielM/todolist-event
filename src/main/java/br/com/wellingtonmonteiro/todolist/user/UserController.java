787c7a75b4ef200f062bbd4efd1617876bf2e8ec
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-10-14 17:49:31 -0300

    adicionando criptografia de senha

--- src/main/java/br/com/wellingtonmonteiro/todolist/user/UserController.java
+++ src/main/java/br/com/wellingtonmonteiro/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificador
  * public
@@ -15,9 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
  * protected
  **/
 
+
+//Camada entre a lógica e a view
+// @Controller // Utiliza quando é criado uma estrutura com páginas, retorno de template, ou seja, flexibilidade maior de retorno
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    /**
+     * Métodos de acesso do HTTP
+     * GET - Buscar uma informção
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar um dado/info
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte da info/dado
+     **/
 
     @Autowired
     private IUserRepository userRepository;
@@ -29,8 +43,14 @@ public class UserController {
 
 
         if(user != null) {
+            // Mensagem de erro
+            // Status Code
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         // Spring já possui o retorno dos status
         var userCreated = this.userRepository.save(userModel);
